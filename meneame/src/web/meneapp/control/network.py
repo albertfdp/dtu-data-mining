769974f79d640506e630f07ce3df730b8ebd39fd
Ferdinando Papale <ferdinando.papale@gmail.com>
2013-12-04 19:34:45 +0100

    Added network discussion on meneameapp

---
+++ meneame/src/web/meneapp/control/network.py
@@ -0,0 +1,18 @@
+import webapp2
+import jinja2
+import os
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), '../view')))
+
+class NetworkHandler(webapp2.RequestHandler):
+    
+    def get(self):
+        
+        template_values = {
+            'title': 'Meneame',
+			'project_name': 'Meneapp'
+        }
+    
+        template = JINJA_ENVIRONMENT.get_template('network.html')
+        self.response.write(template.render(template_values))
